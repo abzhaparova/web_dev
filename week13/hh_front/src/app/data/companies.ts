1ae770be75f128d4f8ce693c071b031d6a2607a2
abzhaparova <43296134+abzhaparova@users.noreply.github.com>
2020-04-16 23:49:21 +0600

    Add files via upload

---
+++ week13/hh_front/src/app/data/companies.ts
@@ -0,0 +1,126 @@
+export interface Company{
+    id:number;
+    name:string;
+    description:string; 
+    city:string;
+    address:string;
+}
+export class LoginResponse{
+    token:string;
+}
+
+
+// export const COMPANIES:Company[]=[
+//     {
+//         "id": 1,
+//         "name": "10Tech",
+//         "description": "The company has the latest innovative equipment, software and the necessary computing power to develop augmented and virtual reality applications.",
+//         "city": "Nur-Sultan(Astana)",
+//         "address": "020000, Nur-Sultan"
+//     },
+//     {
+//         "id": 2,
+//         "name": "3S Consulting",
+//         "description": "3S Consulting was established in 2007. The main activity is the development, implementation and support of turnkey solutions based on 1C Enterprise.",
+//         "city": "Almaty",
+//         "address": "Almaty"
+//     },
+//     {
+//         "id": 3,
+//         "name": "1C Bitrix KZ",
+//         "description": "1C-Bitrix is ​​one of the largest IT companies in Russia and the CIS countries, a leading player in the market of professional systems for managing web projects",
+//         "city": "Almaty",
+//         "address": "050010, Almaty"
+//     },
+//     {
+//         "id": 4,
+//         "name": "3Soft",
+//         "description": "Printing infrastructure - audit, sale, optimization, integration, outsourcing and full service and warranty service (Xerox, Canon, HP, Epson)",
+//         "city": "Astana",
+//         "address": "Astana"
+//     },
+//     {
+//         "id": 5,
+//         "name": "Adaptive",
+//         "description": "Main activity is the development of web sites",
+//         "city": "Astana",
+//         "address": "Astana"
+//     },
+//     {
+//         "id": 7,
+//         "name": "new company added",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 8,
+//         "name": "changed",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 9,
+//         "name": "company 9 changed",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 10,
+//         "name": "changed 3",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 11,
+//         "name": "changed 3",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 12,
+//         "name": "changed 3",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 13,
+//         "name": "changed 4",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 14,
+//         "name": "changed 4",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 15,
+//         "name": "changed 4",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 16,
+//         "name": "changed 4",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     },
+//     {
+//         "id": 17,
+//         "name": "changed 4",
+//         "description": "",
+//         "city": "",
+//         "address": ""
+//     }
+// ];
---
+++ week13/hh_front/src/app/data/vacancies.ts
@@ -0,0 +1,101 @@
+export interface Vacancy{
+  name:string;
+  id:number;
+  description:string
+  salary:number;
+  company:number
+}
+
+// export const VACANCIES:Vacancy[]= [
+//   {
+//     "id": 1,
+//     "name": "Project Manager",
+//     "description": "no description",
+//     "salary": 500000.0,
+//     "company": 3
+// },
+// {
+//     "id": 2,
+//     "name": "Project Manager",
+//     "description": "Analyzing and managing project risk",
+//     "salary": 380000.0,
+//     "company": 1
+// },
+// {
+//     "id": 3,
+//     "name": "Android Developer",
+//     "description": "development of mobile applications",
+//     "salary": 500000.0,
+//     "company": 1
+// },
+// {
+//     "id": 4,
+//     "name": "Director Assistant",
+//     "description": "help to organize work and tasks",
+//     "salary": 130000.0,
+//     "company": 1
+// },
+// {
+//     "id": 5,
+//     "name": "Senior C# Developer",
+//     "description": "knowledge of C#, asp.net",
+//     "salary": 7000000.0,
+//     "company": 2
+// },
+// {
+//     "id": 6,
+//     "name": "Developer assistant",
+//     "description": "entering data into the database, adjusting the work in the trading program ensuring the life of the PC",
+//     "salary": 115000.0,
+//     "company": 2
+// },
+// {
+//     "id": 7,
+//     "name": "iOS Developer",
+//     "description": "development of mobile applications for iOS based systems",
+//     "salary": 650000.0,
+//     "company": 2
+// },
+// {
+//     "id": 8,
+//     "name": "Android Developer",
+//     "description": "development of mobie applications for Android based systems",
+//     "salary": 700000.0,
+//     "company": 2
+// },
+// {
+//     "id": 9,
+//     "name": "1C Developer",
+//     "description": "no description",
+//     "salary": 300000.0,
+//     "company": 4
+// },
+// {
+//     "id": 10,
+//     "name": "iOS Developer",
+//     "description": "development of mobile applications for iOS based systems",
+//     "salary": 450000.0,
+//     "company": 4
+// },
+// {
+//     "id": 11,
+//     "name": "Backend Developer",
+//     "description": "no description",
+//     "salary": 500000.0,
+//     "company": 4
+// },
+// {
+//     "id": 12,
+//     "name": "1C Developer",
+//     "description": "no description",
+//     "salary": 600000.0,
+//     "company": 4
+// },
+// {
+//     "id": 13,
+//     "name": "Frontend Developer",
+//     "description": "no description",
+//     "salary": 800000.0,
+//     "company": 5
+// }
+//   ];
